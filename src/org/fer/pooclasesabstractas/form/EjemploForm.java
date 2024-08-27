2cacda964b43924fcd5333f8c7b616d73161113b
javier fer <javierultimasesion@gmail.com>
2024-08-26 21:31:45 -0600

    implementacion clase main y asignacion de valores

---
+++ src/org/fer/pooclasesabstractas/form/EjemploForm.java
@@ -0,0 +1,33 @@
+package org.fer.pooclasesabstractas.form;
+
+import org.fer.pooclasesabstractas.form.elementos.ElementoForm;
+import org.fer.pooclasesabstractas.form.elementos.SelectForm;
+import org.fer.pooclasesabstractas.form.elementos.TextAreaForm;
+import org.fer.pooclasesabstractas.form.elementos.inputForm;
+import org.fer.pooclasesabstractas.form.elementos.select.Opcion;
+
+public class EjemploForm {
+    public static void main(String[] args) {
+        inputForm username = new inputForm("username");
+        inputForm password = new inputForm("clave", "password");
+        inputForm email = new inputForm("email", "email");
+        inputForm edad = new inputForm("edad", "number");
+
+        TextAreaForm experiencia = new TextAreaForm("exp", 5, 9);
+        SelectForm lenguaje = new SelectForm("Lenguaje");
+        Opcion java = new Opcion("1", "java");
+        lenguaje.addOpcion(java);
+        lenguaje.addOpcion(new Opcion("2", "python"));
+        lenguaje.addOpcion(new Opcion("3", "JavasCript"));
+        lenguaje.addOpcion(new Opcion("4", "TypeScript"));
+        lenguaje.addOpcion(new Opcion("5", "PHP"));
+
+        username.setValor("john doe");
+        password.setValor("a1c2b3");
+        email.setValor("email@gmail.com");
+        edad.setValor("28");
+        experiencia.setValor("........ mas 10 años de experiencia ...........");
+        java.setSelected(true);
+
+    }
+}
--- src/org/fer/pooclasesabstractas/form/elementos/SelectForm.java
+++ src/org/fer/pooclasesabstractas/form/elementos/SelectForm.java
@@ -1,10 +1,41 @@
 package org.fer.pooclasesabstractas.form.elementos;
 
+import org.fer.pooclasesabstractas.form.elementos.select.Opcion;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelectForm extends ElementoForm{
 
-    
+    List<Opcion> opciones;
+
+    public SelectForm(String nombre) {
+        super(nombre);
+        this.opciones = new ArrayList<Opcion>();
+    }
+
+    public SelectForm(String nombre, List<Opcion> opciones) {
+        super(nombre);
+        this.opciones = opciones;
+    }
+    public SelectForm addOpcion(Opcion opcion){
+        this.opciones.add(opcion);
+        return this;
+    }
+
     @Override
     public String dibujarHtml() {
-        return null;
+        StringBuilder sb = new StringBuilder("<select ");
+        sb.append("name='").append(this.nombre).append("'>");
+        for (Opcion opcion: this.opciones){
+            sb.append("\n<option value='").append(opcion.getValor()).append("'");
+                    if (opcion.isSelected()){
+                        sb.append(" selected");
+                        this.valor = opcion.getValor();
+                    }
+                    sb.append(">").append(opcion.getNombre()).append("</option>");
+        }
+        sb.append("</select");
+        return sb.toString();
     }
 }
